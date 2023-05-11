bb93367b9c6beb37f94f3675c21f8f29aff95702
noran <noranCSPro@gmail.com>
2023-05-11 13:56:18 +0300

    commit one

--- app/src/main/java/com/example/noteckv1/CallbackFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.noteckv1;
-
-public interface CallbackFragment {
-    void changeFragment();
-
-}
--- app/src/main/java/com/example/noteckv1/SignUpPage.java
+++ app/src/main/java/com/example/noteckv1/SignUpPage.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SecPage extends AppCompatActivity {
+public class SignUpPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sec_page);
-
+        setContentView(R.layout.activity_sign_up_page);
     }
 }
